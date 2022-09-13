cad5a28b5f1f71709a412331b326894a6db9abc4
Jaime Velez <31224654+Jsvelezm@users.noreply.github.com>
2022-09-13 12:30:20 -0500

    agregando interfaces

---
+++ Proyecto_fundacion/src/main/java/Proyecto_fundacion/controller/Notificaciones.java
@@ -0,0 +1,18 @@
+package Proyecto_fundacion.controller;
+
+import Proyecto_fundacion.models.Estudiantes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Notificaciones {
+    
+    Notificaciones Notificaciones = new Notificaciones();
+    
+}
--- Proyecto_fundacion/src/main/java/Proyecto_fundacion/dao/Conexion.java
+++ Proyecto_fundacion/src/main/java/Proyecto_fundacion/dao/Conexion.java
@@ -12,7 +12,7 @@ public class Conexion {
         
         String url = "jdbc:mysql://localhost:3306/fundacion_nh";
         String user = "root";
-        String password = "";
+        String password = "nemesis159";
          
             System.out.println("----------- Iniciando Conexion -----------");
             
